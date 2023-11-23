e4929381fcedb2c553dd6675edbb6fb304fb567d
dipakvb <145524846+dipakvb@users.noreply.github.com>
2023-11-23 12:15:29 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("inpute-box");
+const listContainer = document.getElementById("list-container");
+
+// *Add text in the app using this function
+
+function Add() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  }
+  else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData();
+  }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask();
